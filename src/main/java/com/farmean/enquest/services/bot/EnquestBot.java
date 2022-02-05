bf7318bde573f238683b3ecb8a319f81dd8d70a1
Aleksandr Ovchinin <alex.ovchinin@gmail.com>
2022-02-05 19:39:54 +0500

    Log every message

--- src/main/java/com/farmean/enquest/services/bot/EnquestBot.java
+++ src/main/java/com/farmean/enquest/services/bot/EnquestBot.java
@@ -9,7 +9,7 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class EnquestBot extends TelegramLongPollingBot {
-    private static final Logger logger = LoggerFactory.getLogger(EnquestBot.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(EnquestBot.class);
 
     private final BotConfiguration botConfiguration;
 
@@ -34,10 +34,12 @@ public class EnquestBot extends TelegramLongPollingBot {
             message.setChatId(update.getMessage().getChatId().toString());
             message.setText(update.getMessage().getText());
 
+            LOGGER.warn("Chat id: {}, text: {}", message.getChatId(), message.getText());
+
             try {
                 execute(message); // Call method to send the message
             } catch (TelegramApiException e) {
-                logger.error("Error while processing message {}", message, e);
+                LOGGER.error("Error while processing message {}", message, e);
             }
         }
     }
